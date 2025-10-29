811eec4e713fb7f17d77640b3922bc37589bc803
FireArtist1902 <vadimvrachenko@gmail.com>
2025-10-29 20:10:30 +0200

    Final

--- Lab9/script.js
+++ Lab9/script.js
@@ -15,17 +15,44 @@ const generateBtn = document.getElementById("btn");
 const computerScore = document.getElementById("computerScore");
 const computerCard = document.getElementById("computerCard");
 
-const values = [2, 3, 4, 6, 7, 8, 9, 10, 11]; // 9 numbers
+const values = [2, 3, 4, 6, 7, 8, 9, 10, 11];
 
 let rounds = 0;
+let playerResult = 0;
+let computerResult = 0;
 
 tryCounter.innerText = `Спроба ${rounds} з 3`
 
 playerName.innerText = name;
 
 generateBtn.addEventListener("click", () => {
-    let tempPlayerScore = Math.floor(Math.random() * 10);
-    let tempComputerScore = Math.floor(Math.random() * 10);
-    playerCard.style.backgroundImage = `url("Images/${values[tempPlayerScore]}R.png")`
-    computerCard.style.backgroundImage = `url("Images/${values[tempComputerScore]}B.png")`
+    let tempPlayerScore = Math.floor(Math.random() * 9);
+    let tempComputerScore = Math.floor(Math.random() * 9);
+
+    playerCard.style.backgroundImage = `url("Images/${values[tempPlayerScore]}R.png")`;
+    computerCard.style.backgroundImage = `url("Images/${values[tempComputerScore]}B.png")`;
+
+    playerResult += values[tempPlayerScore];
+    computerResult += values[tempComputerScore];
+
+    playerScore.innerText = playerResult;
+    computerScore.innerText = computerResult;
+
+    rounds++;
+    tryCounter.innerText = `Спроба ${rounds} з 3`
+
+    if(rounds === 3)
+    {
+        if(playerResult > computerResult)
+        {
+            alert("Виграв гравець");
+            location.reload();
+        }else if(computerResult > playerResult){
+            alert("Виграв комп'ютер");
+            location.reload();
+        } else{
+            alert("Нічия");
+            location.reload();
+        }
+    }
 });
