b11ce7144374b5c589a31147e026a353c0683e81
FireArtist1902 <vadimvrachenko@gmail.com>
2025-10-29 19:53:05 +0200

    Changed Images

--- Lab9/script.js
+++ Lab9/script.js
@@ -1,4 +1,3 @@
-const playerName = document.getElementById("playerName");
 let name = prompt("Введіть ім'я");
 if(name.length < 5)
 {
@@ -6,6 +5,7 @@ if(name.length < 5)
     location.reload();
 }
 
+const playerName = document.getElementById("playerName");
 const playerScore = document.getElementById("playerScore");
 const playerCard = document.getElementById("playerCard");
 
@@ -14,3 +14,18 @@ const generateBtn = document.getElementById("btn");
 
 const computerScore = document.getElementById("computerScore");
 const computerCard = document.getElementById("computerCard");
+
+const values = [2, 3, 4, 6, 7, 8, 9, 10, 11]; // 9 numbers
+
+let rounds = 0;
+
+tryCounter.innerText = `Спроба ${rounds} з 3`
+
+playerName.innerText = name;
+
+generateBtn.addEventListener("click", () => {
+    let tempPlayerScore = Math.floor(Math.random() * 10);
+    let tempComputerScore = Math.floor(Math.random() * 10);
+    playerCard.style.backgroundImage = `url("Images/${values[tempPlayerScore]}R.png")`
+    computerCard.style.backgroundImage = `url("Images/${values[tempComputerScore]}B.png")`
+});
